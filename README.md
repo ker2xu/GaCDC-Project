ba1e1bdfd8bc32a1d18a45b173f58161c1e46dc5
ker2xu <ker2.xu@gmail.com>
2018-03-29 22:20:36 +0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# GaCDC-Project
+# GaCDC-Project(Getting and Cleaning Data Course Project)
+* run_analysis.R - generate the tidy set of step 5 (This file should be executed outside the "UCI HAR Dataset" directory)
+* tidy_set_of_step5.csv - use read.csv(tidy_set_of_step5) to read 
+* CodeBook.R - explain the values in tidy_set_of_step5.csv file
---
+++ run_analysis.R
@@ -0,0 +1,31 @@
+# This file should be executed just outside the "UCI HAR Dataset" folder
+library(dplyr)
+
+# Merge data and give lables to values in x/y/subject data (step 1 & 4)
+act_labels <- read.table("UCI HAR Dataset/activity_labels.txt", sep = " ", row.names = 1, stringsAsFactors = T)
+features <- read.table("UCI HAR Dataset/features.txt", sep = " ", row.names = 1, stringsAsFactors = F)
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "", col.names = features[, 1], stringsAsFactors = F, check.names = F)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", sep = " ", col.names = "activity.id", stringsAsFactors = F)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", sep = " ", col.names = "subject.id", stringsAsFactors = F)
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "", col.names = features[, 1], stringsAsFactors = F, check.names = F)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", sep = " ", col.names = "activity.id", stringsAsFactors = F)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", sep = " ", col.names = "subject.id", stringsAsFactors = F)
+
+train_data_set <- cbind(subject_train, y_train, x_train)
+test_data_set <- cbind(subject_test, y_test, x_test)
+data_set <- rbind(train_data_set, test_data_set)
+
+# Extract mean and std columns from data_set (step 2)
+mean_and_std_cols <- grep("[Mn]ean|[Ss]td", colnames(data_set))
+data_set <- data_set[, c(1, 2, mean_and_std_cols)]
+
+# change integers to corresponding characters (step 3)
+data_set$activity.id <- act_labels[data_set$activity.id, 1]
+data_set <- rename(data_set, activity.name = activity.id)
+
+# step 4 has been done in step 1
+# Get the independent tidy set with dimensions of 180 * 44 (step 5)
+by_subject_id_and_activity_name <- group_by(data_set, subject.id, activity.name) %>%
+  summarise_all(mean)
+
+write.csv(by_subject_id_and_activity_name, file = "tidy_set_of_step5.csv", quote = F, row.names = F)
